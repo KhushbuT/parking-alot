840a99e34263a7b6366cf3bff56aeb672ed020cd
subeer-PC\subeer <khushbuthakur31@gmail.com>
2020-11-18 14:09:50 +0530

    main class of parking lot - accept input

---
+++ src/main/java/com/gojek/main/ParkingLotMain.java
@@ -0,0 +1,15 @@
+package com.gojek.main;
+
+import java.util.Scanner;
+
+public class ParkingLotMain {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your input file name : ");
+		Scanner scanInput = new Scanner(System.in);
+		String fileName = scanInput.next();
+		
+		
+	}
+
+}
