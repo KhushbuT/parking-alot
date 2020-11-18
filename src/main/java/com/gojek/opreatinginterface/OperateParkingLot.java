02a85aebdafb63f5c74d587275f1084f3ed33669
subeer-PC\subeer <khushbuthakur31@gmail.com>
2020-11-18 14:27:02 +0530

    capacity is added

--- src/main/java/com/gojek/opreatinginterface/OperateParkingLot.java
+++ src/main/java/com/gojek/opreatinginterface/OperateParkingLot.java
@@ -1,11 +1,25 @@
 package com.gojek.opreatinginterface;
 
+import java.util.ArrayList;
+
 public class OperateParkingLot implements OperateInterface{
 
+	private static Integer size=0;
+
+	// Available slots list
+   static ArrayList<Integer> availableSlotList;
+    
+	
 	@Override
 	public void createParkingLot(int capacity) {
 		
-		System.out.println("capacity");
+		OperateParkingLot.size = capacity;
+		this.availableSlotList = new ArrayList<Integer>();
+        for (int i=1; i<= this.size; i++) {
+            availableSlotList.add(i);
+        }
+        System.out.println("Created parking lot with " + capacity + " slots");
+        System.out.println();
 		
 	}
 
